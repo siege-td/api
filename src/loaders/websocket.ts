7b5ec068980778cb3ac9fc920b2815759a3fc7fd
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-03-15 14:40:16 +0100

    Add socker room stuff

--- src/loaders/websocket.ts
+++ src/loaders/websocket.ts
@@ -2,6 +2,11 @@ import { Application } from "express";
 import http from 'http'
 import { Server, Socket } from "socket.io";
 
+interface INewLobby {
+    pin: number
+    lobby: string | string[]
+}
+
 export default async (expressApp: Application) => {
     const httpServer = http.createServer(expressApp)
 
@@ -9,8 +14,21 @@ export default async (expressApp: Application) => {
         cors: { origin: '*' }
     })
 
-    socketServer.on('connection', (socket: Socket) => {
-        socket.send('Radadiiiii')
+    const socketRooms: Map<number, string | string[]> = new Map()
+
+    socketServer.on('connection', (socketConnection: Socket) => {
+        console.log(`Socket: ${socketConnection.id} connected`)
+
+        socketConnection.on('new_lobby', (newLobby: INewLobby) => {
+            socketRooms.set(newLobby.pin, newLobby.lobby)
+            socketConnection.join(socketRooms.get(newLobby.pin)!)
+            console.log(socketRooms.get(newLobby.pin))
+        })
+
+        socketConnection.on('join_lobby', (pin: number) => {
+            socketConnection.join(socketRooms.get(pin)!)
+            console.log(socketRooms.get(pin))
+        })
     })
 
     httpServer.listen(process.env.WS_PORT)
