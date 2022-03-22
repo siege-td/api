53f54e786f759be429d5a4acaf7efe90e6253f33
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-03-22 15:10:33 +0100

    Data handling for multiplayer up?

--- src/loaders/websocket.ts
+++ src/loaders/websocket.ts
@@ -2,6 +2,11 @@ import { Application } from "express";
 import http from 'http'
 import { Server, Socket } from "socket.io";
 
+interface IGameData {
+    playerName: string
+    hitpoints: number
+    currency: number
+}
 
 export default async (expressApp: Application) => {
     const httpServer = http.createServer(expressApp)
@@ -11,6 +16,7 @@ export default async (expressApp: Application) => {
     })
 
     const socketRooms: Map<number, string | string[]> = new Map()
+    const gameSessionsData: Map<number, IGameData[]> = new Map()
 
     socketServer.on('connection', (socketConnection: Socket) => {
         console.log(`Socket: ${socketConnection.id} connected`)
@@ -18,17 +24,42 @@ export default async (expressApp: Application) => {
         socketConnection.on('new_lobby', (newPin: number) => {
             const roomName = `room${socketRooms.size}`
             socketConnection.join(roomName)
+
             socketRooms.set(newPin, roomName)
+            gameSessionsData.set(newPin, [{
+                playerName: socketConnection.id,
+                hitpoints: 100,
+                currency: 0
+            }])
         })
 
         socketConnection.on('join_lobby', (pin: number) => {
             socketConnection.join(socketRooms.get(pin)!)
+            const currentData = gameSessionsData.get(pin)
+            
+            currentData?.push({playerName: socketConnection.id,
+                hitpoints: 100,
+                currency: 0}
+            )
+            
+            gameSessionsData.set(pin, currentData!)
         })
 
         socketConnection.on('close_lobby', (pin: number) => {
             // Close socket room
             socketServer.in(socketRooms.get(pin)!).disconnectSockets(true)
             socketRooms.delete(pin)
+            gameSessionsData.delete(pin)
+        })
+
+        socketConnection.on('update_game_data', (pin: number, gameData: IGameData[]) => {
+            // First, check if room exists
+            if (!socketRooms.get(pin)) {
+                socketConnection.emit("error", { message: "Pin invalid" })
+            }
+            // If exists, update data and emit
+            gameSessionsData.set(pin, gameData)
+            socketConnection.to(socketRooms.get(pin)!).emit("updated_data", gameSessionsData.get(pin!))
         })
     })
 
