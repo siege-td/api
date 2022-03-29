91c05e32d88d0a72e431ee5339443c0fd22de179
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-03-29 15:47:06 +0200

    Websocket stuff

--- src/loaders/websocket.ts
+++ src/loaders/websocket.ts
@@ -1,5 +1,5 @@
 import { Application } from "express";
-import http from 'http'
+import http from 'http';
 import { Server, Socket } from "socket.io";
 
 interface IGameData {
@@ -16,7 +16,7 @@ export default async (expressApp: Application) => {
     })
 
     const socketRooms: Map<number, string | string[]> = new Map()
-    const gameSessionsData: Map<number, IGameData[]> = new Map()
+    const gameSessionsData: Map<number, IGameData[] | undefined> = new Map()
 
     socketServer.on('connection', (socketConnection: Socket) => {
         console.log(`Socket: ${socketConnection.id} connected`)
@@ -52,13 +52,38 @@ export default async (expressApp: Application) => {
             gameSessionsData.delete(pin)
         })
 
-        socketConnection.on('update_game_data', (pin: number, gameData: IGameData[]) => {
+        socketConnection.on('update_game_data', (
+            {
+                pin, 
+                playerName, 
+                hitpoints, 
+                currency
+            }: {
+                pin: number, 
+                playerName: string, 
+                hitpoints: number, 
+                currency: number
+            }) => {
             // First, check if room exists
             if (!socketRooms.get(pin)) {
                 socketConnection.emit("error", { message: "Pin invalid" })
+                return
             }
             // If exists, update data and emit
-            gameSessionsData.set(pin, gameData)
+            const tempData = gameSessionsData.get(pin)
+
+            if (tempData?.length === 0) {
+                tempData.push({ playerName, hitpoints, currency })
+                gameSessionsData.set(pin, tempData)
+            } else if (tempData !== undefined) {
+                for (let i = 0; i < tempData.length; i++) {
+                    if (playerName === tempData[i].playerName) {
+                        tempData[i] = { playerName, hitpoints, currency }
+                        gameSessionsData.set(pin, tempData)
+                    }
+                }
+            }
+            console.log(gameSessionsData.get(pin))
             socketConnection.to(socketRooms.get(pin)!).emit("updated_data", gameSessionsData.get(pin!))
         })
     })
